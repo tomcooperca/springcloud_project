19ebfca24ce83bc1bdd7687a698f4c43bc79b143
Thomas Cooper <thomascooperca@gmail.com>
2017-12-04 08:57:43 -0800

    fallback method + test

--- greeting-app/src/main/java/io/pivotal/training/greeting/FortuneServiceClient.java
+++ greeting-app/src/main/java/io/pivotal/training/greeting/FortuneServiceClient.java
@@ -1,5 +1,8 @@
 package io.pivotal.training.greeting;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -10,14 +13,22 @@ import java.util.Map;
 @Component
 public class FortuneServiceClient {
 
+    public static final Logger log = LoggerFactory.getLogger(FortuneServiceClient.class);
+
     @Value("${fortuneService.baseUrl}")
     String baseUrl;
 
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "defaultFortune")
     public String getFortune() {
         Map fortune = restTemplate.getForObject(baseUrl, Map.class);
         return (String) fortune.get("fortune");
+    }
+
+    public String defaultFortune() {
+        log.info("Default fortune used");
+        return "Your future is uncertain";
     }
 }

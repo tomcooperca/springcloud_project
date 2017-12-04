8f0872158e8c1126341a5be9e29cd43276392888
Thomas Cooper <thomascooperca@gmail.com>
2017-12-03 16:28:05 -0800

    consumer contract generation

--- greeting-app/src/test/java/io/pivotal/training/greeting/FortuneServiceClientTests.java
+++ greeting-app/src/test/java/io/pivotal/training/greeting/FortuneServiceClientTests.java
@@ -1,43 +1,23 @@
 package io.pivotal.training.greeting;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@AutoConfigureStubRunner(workOffline = true, ids = "io.pivotal.training.springcloud:fortune-service:+:stubs:8081")
 public class FortuneServiceClientTests {
 
     @Autowired
     FortuneServiceClient fortuneServiceClient;
 
     public static final String expectedFortune = "my test fortune";
-
-    @Rule
-    public WireMockRule wireMockRule = new WireMockRule(wireMockConfig().port(8081));
-
-    @Before
-    public void setup() {
-        stubFor(WireMock.get(urlEqualTo("/"))
-                .willReturn(aResponse()
-                        .withStatus(200)
-                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-                        .withBody(String.format("{\"fortune\": \"%s\"}", expectedFortune))
-                ));
-    }
 
     @Test
     public void shouldReturnAFortune() {

a4e97c0c822153ab15daecb88954b0645ab88bbb
Thomas Cooper <thomascooperca@gmail.com>
2017-12-04 10:21:00 -0800

    eureka lookup from greeting-app to fortune-service (via discoveryClient)

--- greeting-app/src/main/java/io/pivotal/training/greeting/FortuneServiceClient.java
+++ greeting-app/src/main/java/io/pivotal/training/greeting/FortuneServiceClient.java
@@ -1,28 +1,32 @@
 package io.pivotal.training.greeting;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+import java.util.List;
 import java.util.Map;
 
 @Component
+@Slf4j
+@AllArgsConstructor
 public class FortuneServiceClient {
 
-    public static final Logger log = LoggerFactory.getLogger(FortuneServiceClient.class);
-
-    @Value("${fortuneService.baseUrl}")
-    String baseUrl;
-
-    @Autowired
+    DiscoveryClient discoveryClient;
     RestTemplate restTemplate;
 
     @HystrixCommand(fallbackMethod = "defaultFortune")
     public String getFortune() {
+        String baseUrl = lookupUrlFor("FORTUNE");
         Map fortune = restTemplate.getForObject(baseUrl, Map.class);
         return (String) fortune.get("fortune");
     }
@@ -30,5 +34,14 @@ public class FortuneServiceClient {
     public String defaultFortune() {
         log.info("Default fortune used");
         return "Your future is uncertain";
+    }
+
+    private String lookupUrlFor(String app) {
+        List<ServiceInstance> instances = discoveryClient.getInstances(app);
+        return instances.stream()
+                .findFirst()
+                .map(ServiceInstance::getUri)
+                .map(URI::toString)
+                .orElseThrow(RuntimeException::new);
     }
 }
